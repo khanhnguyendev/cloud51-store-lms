846de5cb3f2592a35ec4a2efe3b282d0f0adc302
nguyen.nk <nguyen.nk@gianty.com.vn>
2025-01-02 15:08:31 +0700

    UI: sidebar setup

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,7 +1,102 @@
+import Image from "next/image";
+import MenuLink from "./menuLink/menuLink";
+import styles from "./sidebar.module.css"
+import {
+    MdAnalytics,
+    MdAttachMoney,
+    MdDashboard,
+    MdHelpCenter,
+    MdOutlineSettings,
+    MdPeople,
+    MdShoppingBag,
+    MdSupervisedUserCircle,
+    MdWork
+} from "react-icons/md";
+
+const menuItems = [
+    {
+        title: "Pages",
+        list: [
+            {
+                title: "Dashboard",
+                path: "/dashboard",
+                icon: <MdDashboard />,
+            },
+            {
+                title: "Users",
+                path: "/dashboard/users",
+                icon: <MdSupervisedUserCircle />,
+            },
+            {
+                title: "Products",
+                path: "/dashboard/products",
+                icon: <MdShoppingBag />,
+            },
+            {
+                title: "Transactions",
+                path: "/dashboard/transactions",
+                icon: <MdAttachMoney />,
+            },
+        ],
+    },
+    {
+        title: "Analytics",
+        list: [
+            {
+                title: "Revenue",
+                path: "/dashboard/revenue",
+                icon: <MdWork />,
+            },
+            {
+                title: "Reports",
+                path: "/dashboard/reports",
+                icon: <MdAnalytics />,
+            },
+            {
+                title: "Teams",
+                path: "/dashboard/teams",
+                icon: <MdPeople />,
+            },
+        ],
+    },
+    {
+        title: "User",
+        list: [
+            {
+                title: "Settings",
+                path: "/dashboard/settings",
+                icon: <MdOutlineSettings />,
+            },
+            {
+                title: "Help",
+                path: "/dashboard/help",
+                icon: <MdHelpCenter />,
+            },
+        ],
+    },
+];
+
 const Sidebar = () => {
   return (
-    <div>
-      sidebar
+    <div className={styles.container}>
+        <div className={styles.user}>
+            <Image className={styles.userImage} src="/noavatar.png" alt="" width={50} height={50} />
+            <div className={styles.userDetail}>
+                <span className={styles.username}>Nguyen</span>
+                <span className={styles.userTitle}>Admin</span>
+
+            </div>
+        </div>
+        <ul>
+            {menuItems.map((cat) => (
+                <li key={cat.title}>
+                    <span className={styles.cat}>{cat.title}</span>
+                    {cat.list.map(item => (
+                        <MenuLink item={item} key={item.title}/>
+                    ))}
+                </li>
+            ))}
+        </ul>
     </div>
   )
 }
