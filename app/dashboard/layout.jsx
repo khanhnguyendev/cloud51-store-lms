0f9ba2b536069d42bbc6e91c3276364ff140b60b
Ngô Khánh Nguyên <44081478+khanhnguyendev@users.noreply.github.com>
2025-01-02 20:54:39 +0700

    UI: dashboard design

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,19 +1,19 @@
-import Sidebar from "@/ui/dashboard/sidebar/sidebar"
-import Navbar from "@/ui/dashboard/navbar/navbar"
-import styles from "../ui/dashboard/dashboard.module.css"
+import Sidebar from "@/ui/dashboard/sidebar/sidebar";
+import Navbar from "@/ui/dashboard/navbar/navbar";
+import styles from "../ui/dashboard/dashboard.module.css";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <div className={styles.container}>
       <div className={styles.menu}>
         <Sidebar />
       </div>
-      <div className={styles.menu}>
+      <div className={styles.content}>
         <Navbar />
         {children}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,27 @@
+import styles from "@/ui/dashboard/dashboard.module.css";
+
+import Card from "@/ui/dashboard/card/card";
+import Rightbar from "@/ui/dashboard/rightbar/rightbar";
+import Transactions from "@/ui/dashboard/transactions/transactions";
+import Chart from "@/ui/dashboard/chart/chart";
+
 const Dashboard = () => {
-    return (
-        <div>Dashboard Page</div>
-    )
-}
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transactions />
+        <Chart />
+        <div className={styles.side}>
+          <Rightbar />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
